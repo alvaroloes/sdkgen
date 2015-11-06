b29ee23650c765c9be979c474a88c6656a5e3c66
alvaroloes <alvarolopezespinosa@gmail.com>
2015-11-06 17:26:53 +0000

    Parser: Added more simple test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -20,11 +20,11 @@ var testCases = []TestCase {
 	{
 		name: "Simple. Only response object",
 		spec: []byte(`GET https://www.alvarloes.com/posts/:id/comments/:id
-				<- {
-					"id":"4567",
-					"title":"I like it",
-					"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client laguanges your API target"
-				}`),
+			<- {
+				"id":"4567",
+				"title":"I like it",
+				"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target"
+			}`),
 		expectedApi: &Api {
 			Endpoints:[]Endpoint {
 				{
@@ -43,8 +43,113 @@ var testCases = []TestCase {
 					ResponseBody: map[string]interface{} {
 						"id":"4567",
 						"title":"I like it",
-						"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client laguanges your API target",
+						"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target",
 					},
+				},
+			},
+		},
+		expectedErr: nil,
+	},{
+		name: "Simple. Request and response object",
+		spec: []byte(`POST https://www.alvarloes.com/posts/:id/comments
+			-> {
+				"title":"I like it",
+				"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target"
+			}
+			<- {
+				"id":"4567",
+				"title":"I like it",
+				"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target"
+			}`),
+		expectedApi: &Api {
+			Endpoints:[]Endpoint {
+				{
+					Method: "POST",
+					FullURL: "https://www.alvarloes.com/posts/:id/comments",
+					Resources: []Resource{
+						{
+							Name:"posts",
+							Parameters:[]string{"id"},
+						}, {
+							Name:"comments",
+							Parameters:nil,
+						},
+					},
+					RequestBody: map[string]interface{} {
+						"title":"I like it",
+						"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target",
+					},
+					ResponseBody: map[string]interface{} {
+						"id":"4567",
+						"title":"I like it",
+						"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target",
+					},
+				},
+			},
+		},
+		expectedErr: nil,
+	},{
+		name: "Simple. Response array",
+		spec: []byte(`GET https://www.alvarloes.com/posts/:id/comments
+			<- [
+				{
+					"id":"4567",
+					"title":"I like it",
+					"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target"
+				},{
+					"id":"4567",
+					"title":"I like it too",
+					"body":"I like it, but it seems to be really hard to come up with a powerfull and flexible generator"
+				}
+			]`),
+		expectedApi: &Api {
+			Endpoints:[]Endpoint {
+				{
+					Method: "GET",
+					FullURL: "https://www.alvarloes.com/posts/:id/comments",
+					Resources: []Resource{
+						{
+							Name:"posts",
+							Parameters:[]string{"id"},
+						}, {
+							Name:"comments",
+							Parameters:nil,
+						},
+					},
+					RequestBody:nil,
+					ResponseBody: []interface{} {
+						map[string]interface{} {
+							"id":"4567",
+							"title":"I like it",
+							"body":"I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target",
+						},
+						map[string]interface{} {
+							"id":"4567",
+							"title":"I like it too",
+							"body":"I like it, but it seems to be really hard to come up with a powerfull and flexible generator",
+						},
+
+					},
+				},
+			},
+		},
+		expectedErr: nil,
+	},{
+		name: "Simple. No request nor response",
+		spec: []byte(`DELETE https://www.alvarloes.com/posts/:id`),
+		expectedApi: &Api {
+			Endpoints:[]Endpoint {
+				{
+					Method: "DELETE",
+					FullURL: "https://www.alvarloes.com/posts/:id",
+					Resources: []Resource{
+						{
+							Name:"posts",
+							Parameters:[]string{"id"},
+						},
+					},
+					RequestBody:nil,
+					ResponseBody:nil,
 				},
 			},
 		},
