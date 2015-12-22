76c93598ca7581b4ec907ad8bec6b369ca89285a
alvaroloes <alvarolopezespinosa@gmail.com>
2015-12-22 15:47:57 +0000

    Generator: Created test infrastructure for generator and added a simple test

---
+++ gen/generator_modelsInfo_test.go
@@ -0,0 +1,99 @@
+package gen
+
+import (
+	"testing"
+
+	"github.com/alvaroloes/sdkgen/parser"
+	"github.com/alvaroloes/sdkgen/tests"
+	"github.com/kr/pretty"
+)
+
+const (
+	failModelsInfoFormat = "Test %q: Got wrong models info. Differences are:\n%v"
+)
+
+type modelsInfoTestCase struct {
+	name               string
+	api                *parser.Api
+	expectedModelsInfo map[string]*modelInfo
+}
+
+var modelsInfoTestCases = []modelsInfoTestCase{
+	{
+		name: "Simple. Only one GET endpoint",
+		api: &parser.Api{
+			Endpoints: []parser.Endpoint{
+				{
+					Method:    "GET",
+					URLString: "https://www.alvarloes.com/posts/:id/comments/:id",
+					URL:       tests.MustParseURL("https://www.alvarloes.com/posts/:id/comments/:id"),
+					Resources: []parser.Resource{
+						{
+							Name:       "posts",
+							Parameters: []string{"id"},
+						}, {
+							Name:       "comments",
+							Parameters: []string{"id"},
+						},
+					},
+					RequestBody: nil,
+					ResponseBody: map[string]interface{}{
+						"id":    "4567",
+						"title": "I like it",
+						"body":  "I like this post about api generators. It would be awesome to have a powerfull generator to avoid coding SDKs for all the client languages your API target",
+					},
+				},
+			},
+		},
+		expectedModelsInfo: map[string]*modelInfo{
+			"comments": {
+				Name: "comments",
+				Properties: map[string]property{
+					"id": {
+						Name:    "id",
+						Type:    "",
+						IsArray: false,
+					},
+					"title": {
+						Name:    "title",
+						Type:    "",
+						IsArray: false,
+					},
+					"body": {
+						Name:    "body",
+						Type:    "",
+						IsArray: false,
+					},
+				},
+				EndpointsInfo: []endpointInfo{
+					{
+						Method:  "GET",
+						URLPath: "/posts/:id/comments/:id",
+						SegmentParams: []string{
+							"id",
+							"id",
+						},
+						ResponseType: 0,
+					},
+				},
+			},
+		},
+	},
+}
+
+func TestModelsInfo(t *testing.T) {
+	for _, testCase := range modelsInfoTestCases {
+		gen := Generator{
+			gen:    nil,
+			api:    testCase.api,
+			config: Config{},
+		}
+		gen.extractModelsInfo()
+
+		diffs := pretty.Diff(testCase.expectedModelsInfo, gen.modelsInfo)
+
+		if len(diffs) > 0 {
+			t.Errorf(failModelsInfoFormat, testCase.name, tests.FormattedDiff(diffs))
+		}
+	}
+}
