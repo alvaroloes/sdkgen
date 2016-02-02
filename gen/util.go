ab7f1b4458227a22559071e3d289709806c72b6f
Alvaro Lopez Espinosa <alvaro@theagilemonkeys.com>
2016-02-02 23:30:27 +0000

    [Generator] Added useful functions to templates

--- gen/util.go
+++ gen/util.go
@@ -3,10 +3,18 @@ package gen
 import (
 	"strings"
 	"text/template"
+
+	"github.com/jinzhu/inflection"
 )
 
 var funcMap = template.FuncMap{
 	"lowerFirst": func(s string) string {
 		return strings.ToLower(s[:1]) + s[1:]
+	},
+	"upperFirst": func(s string) string {
+		return strings.ToUpper(s[:1]) + s[1:]
+	},
+	"pluralize": func(s string) string {
+		return inflection.Plural(s)
 	},
 }
